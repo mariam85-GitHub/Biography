5339d1718d05b81c879a3e909f11df9def4f7e39
akinkaya1 <akinkaya.g@gamil.com>
2021-06-30 15:05:14 -0500

    fixes

--- src/Biography.java
+++ src/Biography.java
@@ -6,6 +6,11 @@ public class Biography {
         This will be our actual program that we define author and his books
         In this program we will write the biography of Stefan Zweig, an Austrian novelist.
 
+
+         Write a program that will get information from user and
+         -Print information for the favorite author
+         -Print information of the books of favorite author
+
         Full name = Stefan Zweig
         County = Austria
         Is still alive: No (28 November 1881 – 22 February 1942)
@@ -17,7 +22,6 @@ public class Biography {
         24 Hours in the Life of a Woman     novella         80
          */
 
-
         /*
         Expected result:
         Author's information = [firstName: Stefan, lastName: Zweig, country: Austria, isAlive: false]
@@ -26,6 +30,8 @@ public class Biography {
         [name: The Royal Game, tale: novella, page: 53, author: Stefan Zweig]
         [name: 24 Hours in the Life of a Woman, tale: novella, page: 80, author: Stefan Zweig]
          */
+
+        //YOUR CODE HERE
 
     }
 }
