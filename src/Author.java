7521fc047fd1b159bde5a3c29dd5e85f7c8ba3ee
akinkaya1 <akinkaya.g@gmail.com>
2021-11-18 18:44:41 -0600

    clearing

--- src/Author.java
+++ src/Author.java
@@ -15,25 +15,11 @@ public class Author {
         NOTE: Books must be defined as a List of Objects of Book class not String
     */
     //YOUR CODE HERE
-    String firstName;
-    int lastName;
+
 
     /*
     Override toString() method here that returns Author object information
      */
     //YOUR CODE HERE
 
-
-    @Override
-    public String toString() {
-        return "Author{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName=" + lastName +
-                '}';
-    }
-
-    public static void main(String[] args) {
-        Author a = new Author();
-        System.out.println(a);
-    }
 }
